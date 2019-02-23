11f9aaa959a56dccb4d9f3252133a4b7591d44a9
isaaccho <cho_isaac@hotmail.com>
2019-02-23 00:46:18 -0500

    included texts

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <p>Location:</p>
-        <p>Country:</p>
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather Teller</h1>
+        <p>Find out the temperature, humidity, and conditions</p>
+      </div>
+    );
+  }
+}
+
+export default Header;

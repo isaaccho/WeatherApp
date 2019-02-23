82d3dd36e8b8ab97ef1a1aeb013e74efca961359
isaaccho <cho_isaac@hotmail.com>
2019-02-23 15:06:49 -0500

    made it possible for users to enter city and country

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Form from "./components/Form";
@@ -10,8 +9,10 @@ const API_KEY = "2d030085d740f1d58d6cfd0329851bc5";
 class App extends Component {
   getWeather = async e => {
     e.preventDefault(); //This wil prevent the default behaviour that will happen when the button is pressed
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}&units=metric`
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
     );
     const data = await api_call.json(); //this will convert the api data into a code that is readable for languages using json
     console.log(data);
